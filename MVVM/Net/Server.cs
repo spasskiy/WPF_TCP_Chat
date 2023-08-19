35f952d66ef45a862900089c5253b2085502d2c0
Spasskiy Pavel <spasskiy@mail.com>
2023-08-19 14:33:56 +0300

    Add project files.

---
+++ MVVM/Net/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Chat19Aug.MVVM.Net
+{
+    internal class Server
+    {
+        TcpClient client;
+        string _IP = "127.0.0.1";
+        int port = 31338;
+        public Server() 
+        {
+            client = new TcpClient();
+        }
+        public void ConnectToServer(string username)
+        {
+            if(!client.Connected)
+            {
+                client.Connect(_IP, port);
+            }
+        }
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Chat19Aug.MVVM.Core;
+using Chat19Aug.MVVM.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat19Aug.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+        public string Username { set; get; }
+        private Server server;
+        public MainViewModel() 
+        { 
+            server = new Server();
+            ConnectToServerCommand = new RelayCommand(x => server.ConnectToServer(Username), x => !string.IsNullOrEmpty(Username));
+        }
+    }
+}
